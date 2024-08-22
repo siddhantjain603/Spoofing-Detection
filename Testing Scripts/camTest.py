4200f8a59b6ac7642a84ce1a733fc35b25797d74
Siddhant Jain <jainsiddhant603@gmail.com>
2024-08-22 11:45:29 +0530

    Create camTest.py

---
+++ Testing Scripts/camTest.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    _, frame = cap.read()
+
+    cv2.imshow("image",frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
